38f0532251d66c3e9184044a49dcb364c0fd7640
sjyoo1572 <sjyoo1572@gmail.com>
2021-11-21 23:48:49 +0900

    프레임단위로 자르기 + 각도에 따라 분류

--- real_time_cv.py
+++ real_time_cv.py
@@ -3,7 +3,6 @@ import time
 import numpy as np
 import os
 
-
 def __angle_between(p1, p2):  #두점 사이의 각도:(getAngle3P 계산용) 시계 방향으로 계산한다. P1-(0,0)-P2의 각도를 시계방향으로
     ang1 = np.arctan2(*p1[::-1])
     ang2 = np.arctan2(*p2[::-1])
@@ -23,21 +22,20 @@ def getAngle3P(p1, p2, p3, direction="CW"): #세점 사이의 각도 1->2->3
 
 
 
-
-def proc(module):
+def proc(module, video):
     angle = 0
     xy_1 =()
     xy_14 =()
     xy_8 =()
     
     
-    if module == "coco":
+    if module is "coco":
         protoFile = "./model/coco.prototxt"
         weightsFile = "./model/coco.caffemodel"
         nPoints = 18
         POSE_PAIRS = [ [1,0],[1,2],[1,5],[2,3],[3,4],[5,6],[6,7],[1,8],[8,9],[9,10],[1,11],[11,12],[12,13],[0,14],[0,15],[14,16],[15,17]]
 
-    elif module == "mpi":
+    elif module is "mpi":
         protoFile = "./model/mpi.prototxt"
         weightsFile = "./model/mpi.caffemodel"
         nPoints = 15
@@ -45,21 +43,13 @@ def proc(module):
     inWidth = 368
     inHeight = 368
     threshold = 0.1
-    #input_source = "./input/" + video
-   
-    #print(input_source)
-    cap = cv2.VideoCapture(0)
+    input_source = "./input/" + video
+    print(input_source)
+    cap = cv2.VideoCapture(input_source)
 
-    #hasFrame, frame = cap.read()
-   # vid_writer = cv2.VideoWriter('./output/' + video.split(".")[0] + "_"+ module + '.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame.shape[1],frame.shape[0]))
-    
-   # capture = cv2.VideoCapture(0)
-    # 캡쳐 프레임 사이즈 조절
-    #capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-    #capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
-      
+    hasFrame, frame = cap.read()
+    vid_writer = cv2.VideoWriter('./output/' + video.split(".")[0] + "_"+ module + '.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame.shape[1],frame.shape[0]))
     net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
-    
     while cv2.waitKey(1) < 0:
         t = time.time()
         hasFrame, frame = cap.read()
@@ -92,47 +82,36 @@ def proc(module):
             partB = pair[1]
             if points[partA] and points[partB]:
                 cv2.line(frame, points[partA], points[partB], (0, 255, 255), 3, lineType=cv2.LINE_AA)
-                cv2.circle(frame, points[partA], 8, (0, 255, 0), thickness=-1, lineType=cv2.FILLED)
+                cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
                 cv2.circle(frame, points[partB], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
                 cv2.putText(frame, str(partA), points[partA], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
-                #cv2.putText(frame, str(points[partA]), points[partA], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
                 
-                if partA == 14 and partB == 8:
+                
+            if partA == 14 and partB == 8:
                     xy_1 = points[partA]
                     xy_14 = points[partB]
                      
-                elif partA == 8 and partB == 9:
+            elif partA == 8 and partB == 9:
                     xy_8 = points[partB]
                     
-            if xy_1 != () and xy_8 != () :
-                angle = getAngle3P(xy_1, xy_14, xy_8)
-                #print(angle)
-                #cv2.putText(frame, str(round(angle,2)), xy_14, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
-                
-                if angle>100 and angle < 165:
+            if xy_1 != () and xy_8 != () and xy_14 != ():
+                angle = getAngle3P(xy_8, xy_14, xy_1)
+                cv2.putText(frame, str(round(angle,2)), xy_14, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, lineType=cv2.LINE_AA)
+                xy_1 = ()
+                xy_8 = ()
+                xy_14 = ()
+                if angle>100 and angle < 185:
                     cv2.putText(frame, "GOOD", (50, 50), cv2.FONT_HERSHEY_COMPLEX, .8, (255, 50, 0), 2, lineType=cv2.LINE_AA)
-                    print("GOOD")
                 else:
                     cv2.putText(frame, "BAD", (50, 50), cv2.FONT_HERSHEY_COMPLEX, .8, (255, 50, 0), 2, lineType=cv2.LINE_AA)
-                    print("BAD")
-
+                
         #cv2.putText(frame, "Test Time = {:.2f} sec".format(time.time() - t), (50, 50), cv2.FONT_HERSHEY_COMPLEX, .8, (255, 50, 0), 2, lineType=cv2.LINE_AA)
-        
-  
-        # 이미지 뒤집기, 1=좌우 뒤집기
-       # frame_fliped = cv2.flip(frame, 0)
-        
-        cv2.imshow("VideoFrame", frame)
-        
-        #vid_writer.write(frame)
-    #vid_writer.release()
-    # 카메라 객체 반환
-    cap.release()
- 
-    # 화면에 나타난 윈도우들을 종료
-    cv2.destroyAllWindows()
-
-
+        vid_writer.write(frame)
+    vid_writer.release()
 
 if __name__ == '__main__':
-    proc("mpi")
+    module = ["mpi","coco"]
+    for i in module:
+        for j in os.listdir("./input"):
+            if j[-3:] in ['mp4','avi']:
+                proc(i, j)
